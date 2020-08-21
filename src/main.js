9ac98acfb4d18553a4c5cb833941766d137092b6
qinjinmei118 <1361661385@qq.com>
2020-08-21 23:24:30 +0800

    登录功能“

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './plugins/element.js'
+import '@/plugins/element.js'
+import '@/assets/global.css'
+import './assets/font-awesome-4.7.0/css/font-awesome.css'
+import store from './store/index'
+import axios from 'axios'
+Vue.prototype.axios=axios;
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,43 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+const Login = () => import('@/components/login/Login')
+const Home=()=>import('@/views/home/Home')
+
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
     path: '/',
-
+    redirect:'/login'
   },
+  {
+    path:'/login',
+    component:Login
+  },
+  {
+   path:'/home',
+    component:Home
+  }
 
 ]
 
 const router = new VueRouter({
+  mode:'history',
   routes
 })
-
+//挂载导航守卫，判断用户是否登录，并赋予权限
+router.beforeEach((to,from,next)=>{
+  if(to.path==='/login'){
+    //放行
+    return next();
+  }
+  const token=window.sessionStorage.getItem('token')
+  if(token){
+    next();
+  }else{
+    //强制跳转
+    return next('/login')
+  }
+})
 export default router
